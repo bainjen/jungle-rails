411f26f8c98de8003a58a2d4b245481d0ab6f873
bainjen <jennifer.fleming.bain@gmail.com>
2020-10-20 03:21:40 +0000

    implement 5 tests for product creation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '#save' do
+    it 'ensures that a product with all four fields set will indeed save successfully' do
+      @category = Category.new(name: "Furniture")
+      @category.save 
+
+      @product = Product.new(name: "cat", price: 8838, quantity: 45, category: @category)
+      @product.save
+      expect(@product).to be_valid
+    end
+
+    it 'should not create a product if a name does not exist' do
+      @category = Category.new(name: "Furniture")
+      @category.save
+
+      @product = Product.new(name: nil, price: 8838, quantity: 45, category: @category)
+      @product.save
+      # puts @product.errors.full_messages
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should not create a product if a price does not exist' do
+      @category = Category.new(name: "Furniture")
+      @category.save
+
+      @product = Product.new(name: "cat", price: nil, quantity: 45, category: @category)
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'should not create a product if a quanity does not exist' do
+      @category = Category.new(name: "Furniture")
+      @category.save
+
+      @product = Product.new(name: "cat", price: 4546, quantity: nil, category: @category)
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should not create a product if a quanity does not exist' do
+      @category = Category.new(name: "Furniture")
+      @category.save
+
+      @product = Product.new(name: "cat", price: 4546, quantity: 78, category: nil)
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+  end
 end
